88025b63b13c0a0dac461f79a0f089561ea9fac5
Lê Đạt <ledatkma@gmail.com>
2022-02-21 12:08:31 +0700

    fix error

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request
 from modules import jobs
 from datetime import datetime
+import sqlite3
 
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def home():
-    dateTime = datetime.now().strftime("%d - %m - %Y, %H:%M:%S")
-    data = jobs.main()
-    return render_template('index.html', data = data, date_time = dateTime)
+    date_time = datetime.now().strftime("%d - %m - %Y, %H:%M:%S")
+    jobs.main()
+    conn = sqlite3.connect('jobs.db')
+    data = conn.execute('SELECT * from jobs')
+    jobs_db = list(data.fetchall())
+    conn.close()
+    return render_template('index.html', data = jobs_db, date_time = date_time)
 
 
 if __name__ == "__main__":
